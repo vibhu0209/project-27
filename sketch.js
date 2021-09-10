5791626109a8675a03331b951d00e2643fc1769f
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2021-09-10 10:18:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var bob1, bob2, bob3, bob4, bob5;
+var bob1;
 var roof ;
-var chain1 ,chain2 , chain3 , chain4 , chain5;
+var chain1;
 
 function preload()
 {
@@ -24,16 +24,10 @@ function setup() {
 	roof = new Ground(400, 200, 300 , 20);
 
 	bob1 = new Bob(400, 400 );
-	bob2 = new Bob(450, 400 );
-	bob3 = new Bob(350, 400 );
-	bob4 = new Bob(500, 400 );
-	bob5 = new Bob(300, 400 );
+	
 
 	chain1 = new Chain(bob1.body, roof.body, 0, 0)
-	chain2 = new Chain(bob2.body, roof.body, 50,0)
-	chain3 = new Chain(bob3.body, roof.body, -50,0)
-	chain4 = new Chain(bob4.body, roof.body, 100,0)
-	chain5 = new Chain(bob5.body, roof.body, -100,0)
+	
 	
 	Engine.run(engine)
 
@@ -46,18 +40,12 @@ function draw() {
   //Engine.update(engine); 
 
   bob1.display();
-  bob2.display();
-  bob3.display();
-  bob4.display();
-  bob5.display();
+ 
 
   roof.display();
 
   chain1.display();
-  chain2.display();
-  chain3.display();
-  chain4.display();
-  chain5.display();
+ 
 
   drawSprites();
 }
